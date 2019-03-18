d4dfd4ac0f94ca6429242d5a531abec0e013921d
Develop Ix <>
2019-03-18 10:39:12 +0700

    Create dices

--- src/example.js
+++ src/example.js
@@ -1,3 +1,18 @@
 export function dices(values) {
+
+  let dice1 = [1, 2, 3, 4, 5, 6]; 
+  let dice2 = [1, 2, 3, 4, 5, 6];
+  let dice3 = [1, 2, 3, 4, 5, 6];
+
+  
+
+  let i = 0
+  let i1 = 0;
+  let i2 = 0;
+  let i3 = 0;
+  while (values !== dice1[i1] + dice2[i2] + dice3[i3]) {
+    
+  }
+
   return "1";
 }
