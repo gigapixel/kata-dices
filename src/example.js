80e4b4d106fe77a4b6d570998ed4ec92105dfdb3
Nopparit.P <nopparip@scg.com>
2019-03-18 10:36:53 +0700

    aaa

--- src/example.js
+++ src/example.js
@@ -1,3 +1,38 @@
-export function  alwaysReturn1() {
+//3-18
+
+let score = 15;
+
+let nDic = 3;
+let dic = [1,2,3,4,5,6];
+let maxScore = 6;
+
+let result = [];
+
+while(score > 0) {
+  if(score > 6) {
+    result.push(6);
+    score = score - 6;
+    nDic--;
+  } else{
+    result.push(score - nDic + 1);
+    score = score - (score - nDic + 1);
+    nDic--;
+    //console.log('score:' + score);
+  }
+
+}
+
+console.log(result);
+
+return;
+
+for(i = 0; i < dic; i ++) {
+  console.log(i);
+  //result.push(arr[i]);
+}
+
+
+
+function alwaysReturn1() {
   return 1;
 }
