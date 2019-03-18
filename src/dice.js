e36dab4da36dd3c92099d37b5a257313ea918634
Wanatchapong Bew <bewbangluang@gmail.com>
2019-03-19 00:05:42 +0700

    update dice.js

---
+++ src/dice.js
@@ -0,0 +1,24 @@
+const _ = require('lodash');
+
+function dices(num) {
+  if (num < 3 || num > 18) {
+    throw new Error('Impossible!');
+  }
+
+  let results = [];
+  for (let i = 1; i <= 6; i++) {
+    for (let j = 1; j <= 6; j++) {
+      for (let k = 1; k <= 6; k++) {
+        if ((i + j + k) === num) {
+          results.push([i, j, k].sort());
+        }
+      }
+    }
+  }
+
+  return _.uniqWith(results, _.isEqual);
+}
+
+// console.log(dices(3));
+// console.log(dices(7));
+console.log(dices(12));
