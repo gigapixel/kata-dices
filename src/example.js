dc8ba3726eff11d84876cb001e1d01fef2a6a581
Develop Ix <>
2019-03-19 10:41:38 +0700

    update code

--- src/example.js
+++ src/example.js
@@ -1,18 +1,20 @@
 export function dices(values) {
-
-  let dice1 = [1, 2, 3, 4, 5, 6]; 
+  let retArr = [];
+  let dice1 = [1, 2, 3, 4, 5, 6];
   let dice2 = [1, 2, 3, 4, 5, 6];
   let dice3 = [1, 2, 3, 4, 5, 6];
 
+  for (let i of dice1){
+    for (let j of dice2){
+      for (let k of dice3){
+        if ((i + j + k) == values){
+          let result = `${i} + ${j} + ${k}`;
+          retArr.push(result);
+        }
+      }
+    }
+  }
   
 
-  let i = 0
-  let i1 = 0;
-  let i2 = 0;
-  let i3 = 0;
-  while (values !== dice1[i1] + dice2[i2] + dice3[i3]) {
-    
-  }
-
-  return "1";
+  return retArr;
 }
